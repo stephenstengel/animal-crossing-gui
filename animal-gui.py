5d93ad4d0e5801b224893cb6a66ec08c99293824
Stephen <stephen.stengel@cwu.edu>
2022-05-12 10:56:06 -0700

    Created a basic glade file for the setup of the window.

---
+++ animal-gui.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  
+#  Copyright 2022 Stephen Stengel <stephen.stengel@cwu.edu> and friends
+#  
+
+
+
+
+def main(args):
+	print("hello")
+	return 0
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
