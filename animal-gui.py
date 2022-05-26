c5bfe2bd6524a5a1254ac0bdb6405451df844310
Stephen <stephen.stengel@cwu.edu>
2022-05-25 23:34:05 -0700

    Tried to add splash screen, but it seems more involved than I thought. Will have to table this for now.

--- animal-gui.py
+++ animal-gui.py
@@ -5,18 +5,56 @@
 #  Copyright 2022 Stephen Stengel <stephen.stengel@cwu.edu> and friends
 #  
 
-
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-from FileSelectWindow import FileSelectWindow
-
 
 def main(args):
-	print("hello")
+	print("Hello! Loading...")
 	
-	theWindow = FileSelectWindow()
+	#Splash screen should go here.
+	#I can't find any working tutorials or documentation. Gtk.main()
+	#makes the window show, BUT blocks execution until you close the splash window.
+
+	# ~ splash = Gtk.Window(title="Hello World")
+	# ~ splash.connect("destroy", Gtk.main_quit)
+	
+	# ~ splash.show_all()
+	
+	# ~ while Gtk.events_pending():
+		# ~ Gtk.main_iteration()
+	# ~ splash.show_all()
+	# ~ splash.show()
+	# ~ Gtk.main_iteration()
+	
+
+	#testing opening another window after doing some heavy imports.
+	# ~ print("loading tensorflow...")
+	# ~ import tensorflow as tf
+	# ~ print("loading sklearn...")
+	# ~ import sklearn
+	# ~ print("loading skimage...")
+	# ~ import skimage
+	# ~ print("loading numpy...")
+	# ~ import numpy as np
+	# ~ print("loading keras.models...")
+	# ~ from keras.models import Sequential
+	# ~ print("loading inceptionv3...")
+	# ~ from keras.applications.inception_v3 import InceptionV3
+	# ~ print("loading keras layers...")
+	# ~ from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+	# ~ print("loading keras loss...")
+	# ~ from keras.losses import SparseCategoricalCrossentropy
+	
+	print("loading file window...")
+	from FileSelectWindow import FileSelectWindow
+	
+	print("Done!")
+	# ~ splash.destroy()
+	
+
+	theWindow2 = FileSelectWindow()
 	Gtk.main()
 
 	return 0
