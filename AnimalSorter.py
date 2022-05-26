1ea165c3443394a562182d4e2522f19218c5d33a
Stephen <stephen.stengel@cwu.edu>
2022-05-25 21:38:02 -0700

    Renamed animal-sorter.py to AnimalSorter.py so that we can import from it.

--- AnimalSorter.py
+++ AnimalSorter.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
+#  animal-sorter.py
+#
+#  Sorts the animal pictures into folders based on a trained tensorflow model.
 #  
 #  Copyright 2022 Stephen Stengel <stephen.stengel@cwu.edu> and friends
 #  
 
 
 import os
-import models
 import shutil
 
+from models import inceptionResNetModel
 
 
 CLASS_BOBCAT = 0
@@ -33,12 +36,15 @@ CLASS_NOT_INTERESTING_STRING = "not"
 CLASS_NAMES_LIST_INT = [CLASS_BOBCAT, CLASS_COYOTE, CLASS_DEER, CLASS_ELK, CLASS_HUMAN, CLASS_NOT_INTERESTING, CLASS_RACCOON, CLASS_WEASEL]
 CLASS_NAMES_LIST_STR = [CLASS_BOBCAT_STRING, CLASS_COYOTE_STRING, CLASS_DEER_STRING, CLASS_ELK_STRING, CLASS_HUMAN_STRING, CLASS_NOT_INTERESTING_STRING, CLASS_RACCOON_STRING, CLASS_WEASEL_STRING]
 
+
+## We really should make a config file so that we don't need all these globals. ##
 IMG_WIDTH = 100
 IMG_HEIGHT = 100
 IMG_CHANNELS = 3
 
 
-
+#should I use command line args? or save the requested folders to temp files?
+#Actually, I can just import the functions from this file and call them directly!
 
 
 
