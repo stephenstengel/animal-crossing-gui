df64c95d937537844e410c354fee774e96ccb0b8
Stephen <stephen.stengel@cwu.edu>
2022-05-25 18:55:31 -0700

    Added documenting comments.

--- FileChooser.py
+++ FileChooser.py
@@ -12,20 +12,23 @@ from gi.repository import Gtk
 
 class FileChooser():
 	def __init__(self, titleText):
-		self.pathStr = None
+		self.pathStr = None #Stores the returned path.
 		self.titleText = titleText
 
-#maybe the below should be in a function for clarity?
+		#Sets up a file chooser dialog window object.
+		dialog = Gtk.FileChooserDialog( \
+				self.titleText, \
+				None, \
+				Gtk.FileChooserAction.SELECT_FOLDER, \
+				(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
 
-		dialog = Gtk.FileChooserDialog(self.titleText, None,
-			Gtk.FileChooserAction.SELECT_FOLDER,
-			(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-			 Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
-
+		#Opens the dialog, waits for the return value.
 		response = dialog.run()
 
+		#Only save the path if the user clicked OK and not cancel.
 		if response == Gtk.ResponseType.OK:
 			self.pathStr = dialog.get_filename()
 
+		#Close the dialog.
 		dialog.destroy()
 
