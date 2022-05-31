2431ba24168279a03671e86348faf59a3191a7eb
Stephen <stephen.stengel@cwu.edu>
2022-05-31 02:31:22 -0700

    Added try catch to accomodate different tensorflow versions.

--- AnimalSorter.py
+++ AnimalSorter.py
@@ -210,8 +210,12 @@ def createDatasetFromImages(sourceFolderStr):
 	
 	from tensorflow import data
 	AUTOTUNE = data.AUTOTUNE
-	# ~ from tensorflow.keras.layers import Rescaling
-	from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+	
+	try:
+		from tensorflow.keras.layers import Rescaling
+	except:
+		from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+
 	normalization_layer = Rescaling(1./255) #for newer versions of tensorflow
 	out_ds = out_ds.map(lambda x: normalization_layer(x),  num_parallel_calls=AUTOTUNE)
 	
