659f0bda4c25ed9f813188225781874adc977c37
Stephen <stephen.stengel@cwu.edu>
2022-05-31 07:00:26 -0700

    Added checking if sort is over.

--- SorterWindow.py
+++ SorterWindow.py
@@ -15,7 +15,6 @@ from tkinter import filedialog
 
 import os
 import threading
-
 from functools import partial
 
 from AnimalSorter import sortAnimalsIntoFolders
@@ -26,22 +25,27 @@ from AnimalSorter import sortAnimalsIntoFolders
 STEPHEN_DEBUG = True
 
 class SorterWindow:
+	#I like setting these here because they will cause error messages if something goes wrong. --Stephen
 	sourceStr = None
 	sourceStr_entry = ""
 	destStr = None
 	destStr_entry = ""
 	mainframe = None
 	myProgressBar = None
-	
+	myRunButton = None
+	mySortThread = None
+	waitTime = None
 
 	def __init__(self, root):
-		root.title("Animal Sorter!")
+		self.waitTime = 2000
+		self.root = root
+		self.root.title("Animal Sorter!")
 
-		self.mainframe = ttk.Frame(root, padding="10 10 10 10")
+		self.mainframe = ttk.Frame(self.root, padding="10 10 10 10")
 		self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-		root.columnconfigure(0, weight=1)
-		root.rowconfigure(0, weight=1)
+		self.root.columnconfigure(0, weight=1)
+		self.root.rowconfigure(0, weight=1)
 
 		self.mainframe.columnconfigure(0, weight=0)
 		self.mainframe.rowconfigure(0, weight=0)
@@ -60,7 +64,6 @@ class SorterWindow:
 		self.destStr_entry = ttk.Entry(self.mainframe, width=100, textvariable=self.destStr)
 		self.destStr_entry.grid(column=2, row=2, sticky=(W, E))
 
-		# ~ ttk.Button(self.mainframe, text="Source Folder", command=self.openSourceDialog).grid(column=3, row=1, sticky=W)
 		sourceTitle = "Choose a source folder..."
 		sourceCommand = partial(self.openFolderDialog, self.sourceStr, sourceTitle)
 		ttk.Button(self.mainframe, text="Source Folder", command = sourceCommand).grid(column=3, row=1, sticky = W + E)
@@ -74,7 +77,8 @@ class SorterWindow:
 		self.myProgressBar.grid(column = 2, row = 3, sticky = W + E)
 		
 		#run button
-		ttk.Button(self.mainframe, text="Run Sorter", command=self.runSorting).grid(column=3, row=3, sticky = W + E)
+		self.myRunButton = ttk.Button(self.mainframe, text="Run Sorter", command=self.runSorting)
+		self.myRunButton.grid(column=3, row=3, sticky = W + E)
 		
 		#Add a little padding to each widget
 		for child in self.mainframe.winfo_children(): 
@@ -112,11 +116,30 @@ class SorterWindow:
 		#run the function
 		
 		self.myProgressBar.start()
-		threading.Thread(target = sortAnimalsIntoFolders, args = (source, dest,)).start()
+		self.myRunButton.state(["disabled"])
+		
 		# ~ sortAnimalsIntoFolders(source, dest)
-		
-		
-		
+		# ~ self.mySortThread = threading.Thread(target = self.testFunction, args = (source, dest,))
+		self.mySortThread = threading.Thread(target = sortAnimalsIntoFolders, args = (source, dest,))
+		self.mySortThread.start()
+		self.root.after(self.waitTime, self.threadChecker)
+
+
+	#Check if the sorting is done every few seconds.
+	def threadChecker(self):
+		print("Threadchecker called!")
+		if self.mySortThread.is_alive():
+			self.root.after(self.waitTime, self.threadChecker)
+		else:
+			self.resetAfterComplete()
+
+
+	def resetAfterComplete(self):
+		self.myProgressBar.stop()
+		self.mySortThread = None
+		self.myRunButton.state(["!disabled"])
+		print("reset complete")
+
 
 	def testFunction(self, *args):
 		try:
